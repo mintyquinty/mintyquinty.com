b3b3840ba0ba7b102398e110428010b93f46e8a0
mintyquinty <quintan.park@gmail.com>
2015-12-20 13:35:30 -0800

    added checks for missing images

--- app/helpers/reddit_views_helper.rb
+++ app/helpers/reddit_views_helper.rb
@@ -1,2 +1,18 @@
 module RedditViewsHelper
+  
+  def get_image_source path
+    src = 'mintyreddit.png'
+    case path
+    when 'nsfw'
+      src = 'mintyreddit_nsfw.png'
+    when 'self'
+      src = 'mintyreddit.png'
+    when ''
+      src = 'mintyreddit.png'
+    else
+      src = path
+    end
+    src
+  end
+  
 end
